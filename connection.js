648193b4e35b5758367ea06645fc45224d1fa2f0
DJJAR24 <DDJA2145@outlook.es>
2024-05-15 23:47:35 -0600

    commit message

--- connection.js
+++ connection.js
@@ -1,4 +1,5 @@
-const oracledb = require('oracledb');
+import oracledb from 'oracledb';
+//import { config } from "./src/config.js"; 
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_11_2' });
 
@@ -7,15 +8,20 @@ async function run() {
 
     try {
         con = await oracledb.getConnection({
-            user: "hr",
-            password: "hr",
-            connectString: "127.0.0.1:1521/XE"
+            user: "db_proyecto_final",
+            password: "its",
+            connectString:"127.0.0.1:1521/XE"
         });
         console.log('Conexión exitosa');
         const data = await con.execute(
-            "SELECT * FROM departments"
+            `INSERT INTO libro (libro_id, titulo, autor, publicacion, editorial) VALUES (:1, :2, :3, :4, :5)`,
+            [1, 'El principito', 'Antoine de Saint-Exupéry', new Date('1943-04-06'), 'Reynal & Hitchcock']
+        );
+        const data1 = await con.execute(
+            'select * from libro'
         );
         console.log(data.rows);
+        console.log(data1.rows);
     } catch (err) {
         console.log(err);
     } finally {
---
+++ src/config.js
@@ -0,0 +1,8 @@
+
+const config={
+    user: process.env.user || '',
+    password:process.env.password || '',
+    connectString: process.env.connectString || ''
+    
+};
+export default config;
